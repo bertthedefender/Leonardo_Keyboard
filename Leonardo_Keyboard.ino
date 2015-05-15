fd6cf55f19f660a31b2129906273f8287ab04a58
unknown <simon@armytruck.co.uk>
2015-05-15 23:54:46 +0100

    Added a 10ms debounce strategy

--- Leonardo_Keyboard.ino
+++ Leonardo_Keyboard.ino
@@ -1,18 +1,18 @@
 
 /*******
  NOTES:
- 
+
  Arduino Leonardo ZX Spectrum keyboard interface (c) 2015 Simon McDonough
- 
+
  The 8 "columns" of the spectrum keyboard matrix are mapped to pins 0-7
  The 5 "rows" of the matrix are mapped to pins 8-12
- 
+
  Each of "columns" are set high using the internal pullup resistor and
  are shorted to ground when a key is pressed.
- 
+
  PIN 13 MUST BE SHORTED TO GROUND IN ORDER TO SEND KEY PRESSES
  THIS IS SO THAT THE KEYBOARD INTERFACE CAN BE TURNED OFF
- 
+
 ********/
 
 int colStartPin = 0;
@@ -26,7 +26,7 @@ int useKeyboardPin = 13;      //If this goes LOW then send the keyboard signal
 void setup() {
 
   Keyboard.begin();
-  
+
   //Clear pins
   for (int row = 0; row < 5; row++) {
     pinMode(rowStartPin + row, OUTPUT);
@@ -38,10 +38,10 @@ void setup() {
   }
 
   for (int x = 0; x < 40; x++) {
-    previousState[x] = 0; 
+    previousState[x] = 0;
     currentState[x] = 0;
   }
-  
+
   pinMode(useKeyboardPin, INPUT_PULLUP);
 }
 
@@ -56,9 +56,9 @@ void setRowHigh(int rowPin) {
 
 
 char getKeyValue(int keyPos) {
-  
+
   int retVal;
-  
+
   switch (keyPos)
   {
     case 0:  retVal = '1';  break;
@@ -104,24 +104,24 @@ char getKeyValue(int keyPos) {
     default:
       retVal = 0;
   }
-  
+
   return retVal;
 }
 
 void pressKey(int keyPos) {
-  
+
   if (digitalRead(useKeyboardPin) == LOW) {
     Keyboard.press(getKeyValue(keyPos));
-  }  
+  }
 }
 
 void releaseKey(int keyPos) {
-  
+
   if (digitalRead(useKeyboardPin) == LOW) {
     Keyboard.release(getKeyValue(keyPos));
   }
 
-  
+
 }
 
 void loop() {
@@ -129,27 +129,33 @@ void loop() {
   //Scan keyboard matrix
   for (int row = 0; row < 5; row++) {
     setRowHigh(rowStartPin + row);
-    for (int col = 0; col < 8; col++) {   
+    for (int col = 0; col < 8; col++) {
       if (digitalRead(col) == LOW) {
-        currentState[row * 8 + col] = 1;
+        delay(10);                          // debounce delay
+        if (digitalRead(col) == LOW) {
+          currentState[row * 8 + col] = 1;
+        }
+        else {
+          currentState[row * 8 + col] = 0;
+        }
       }
       else {
         currentState[row * 8 + col] = 0;
-      }      
+      }
     }
   }
 
-  for (int x=0; x<40; x++) {
+  for (int x = 0; x < 40; x++) {
     if (currentState[x] && !previousState[x])
-       pressKey(x);
-    
+      pressKey(x);
+
     if (previousState[x] && !currentState[x])
-       releaseKey(x);
-     
-    previousState[x] = currentState[x];  
+      releaseKey(x);
+
+    previousState[x] = currentState[x];
   }
-  
-  
+
+
 
 }
 
