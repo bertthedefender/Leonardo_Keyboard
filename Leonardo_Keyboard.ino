19998b00fe88730f376a32544c5538290cce730c
unknown <simon@armytruck.co.uk>
2015-05-15 22:47:58 +0100

    Added keymap for FUSE emulator

--- Leonardo_Keyboard.ino
+++ Leonardo_Keyboard.ino
@@ -34,9 +34,63 @@ void setRowHigh(int rowPin) {
   digitalWrite(rowPin, LOW);
 }
 
+
+int getKeyValue(int keyPos) {
+  
+  int retVal;
+  
+  switch (keyPos)
+  {
+    case 0:  retVal = '1';  break;
+    case 1:  retVal = 'q';  break;
+    case 2:  retVal = 'a';  break;
+    case 3:  retVal = '0';  break;
+    case 4:  retVal = 'p';  break;
+    case 5:  retVal = KEY_LEFT_SHIFT; break;
+    case 6:  retVal = KEY_RETURN; break;
+    case 7:  retVal = ' ';  break;
+    case 8:  retVal = '2';  break;
+    case 9:  retVal = 'w';  break;
+    case 10:  retVal = 's';  break;
+    case 11:  retVal = '9';  break;
+    case 12:  retVal = 'o';  break;
+    case 13:  retVal = 'z';  break;
+    case 14:  retVal = 'l';  break;
+    case 15:  retVal = KEY_LEFT_CTRL;  break;        // Symbol shift
+    case 16:  retVal = '3';  break;
+    case 17:  retVal = 'e';  break;
+    case 18:  retVal = 'd';  break;
+    case 19:  retVal = '8';  break;
+    case 20:  retVal = 'i';  break;
+    case 21:  retVal = 'x';  break;
+    case 22:  retVal = 'k';  break;
+    case 23:  retVal = 'm';  break;
+    case 24:  retVal = '4';  break;
+    case 25:  retVal = 'r';  break;
+    case 26:  retVal = 'f';  break;
+    case 27:  retVal = '7';  break;
+    case 28:  retVal = 'u';  break;
+    case 29:  retVal = 'c';  break;
+    case 30:  retVal = 'j';  break;
+    case 31:  retVal = 'n';  break;
+    case 32:  retVal = '5';  break;
+    case 33:  retVal = 't';  break;
+    case 34:  retVal = 'g';  break;
+    case 35:  retVal = '6';  break;
+    case 36:  retVal = 'y';  break;
+    case 37:  retVal = 'v';  break;
+    case 38:  retVal = 'h';  break;
+    case 39:  retVal = 'b';  break;
+    default:
+      retVal = 0;
+  }
+  
+  return retVal;
+}
+
 void pressKey(int keyPos) {
   
-  Serial.println("Key pressed " + keyPos);
+  Serial.println("Key pressed " +  char(getKeyValue(keyPos)));
   
 }
 
